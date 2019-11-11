1e82cb3804b9c636388f33b3a5d578ae7060bbe8
ankur <s.ankursonawane@gmail.com>
2019-11-11 17:58:42 +0530

    Fix #6

--- animate.py
+++ animate.py
@@ -6,7 +6,7 @@ from matplotlib import pyplot as plt
 plt.rcParams["figure.figsize"] = (20, 3)
 
 
-def plot_daily(*dfs, index=None):
+def plot_daily(*dfs, index=None, save =None):
     if index is not None:
         dfs = [pd.Series(df, index=index) for df in dfs]
     plt.ioff()
