0c31bcc4fddc42aa0716775d26d16fff239f3355
Vikas Baghel <vikasbaghel26@gmail.com>
2019-11-26 23:55:34 +0530

    decompose added

--- utility.py
+++ utility.py
@@ -5,12 +5,12 @@ def split_sequence(sequence, n_steps, n_forward=1):
     X, y = list(), list()
     for i in range(len(sequence)):
         # find the end of this pattern
-        end_ix = i + n_steps + n_forward -1
+        end_ix = i + n_steps 
         # check if we are beyond the sequence
-        if end_ix > len(sequence) - 1:
+        if end_ix + n_forward -1 > len(sequence) - 1:
             break
         # gather input and output parts of the pattern
-        seq_x, seq_y = sequence[i:end_ix], sequence[end_ix]
+        seq_x, seq_y = sequence[i:end_ix], sequence[end_ix+ n_forward -1]
         X.append(seq_x)
         y.append(seq_y)
     return np.array(X), np.array(y)
