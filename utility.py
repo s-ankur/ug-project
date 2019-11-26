47293e44553e12efabb3b092219dc58c1a3db1c4
ankur <s.ankursonawane@gmail.com>
2019-11-26 22:45:35 +0530

    Added plot history

--- utility.py
+++ utility.py
@@ -1,11 +1,11 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-
-def split_sequence(sequence, n_steps):
+def split_sequence(sequence, n_steps, n_forward=1):
     X, y = list(), list()
     for i in range(len(sequence)):
         # find the end of this pattern
-        end_ix = i + n_steps
+        end_ix = i + n_steps + n_forward -1
         # check if we are beyond the sequence
         if end_ix > len(sequence) - 1:
             break
@@ -14,3 +14,12 @@ def split_sequence(sequence, n_steps):
         X.append(seq_x)
         y.append(seq_y)
     return np.array(X), np.array(y)
+
+def plot_history(history):
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('Model loss')
+    plt.ylabel('Loss')
+    plt.xlabel('Epoch')
+    plt.legend(['Train', 'Test'], loc='upper left')
+    plt.show()
